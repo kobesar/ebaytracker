39c8759a5595c48651b8353d95fcd93e14ed3db4
kobesar <klobby19@gmail.com>
2023-01-02 08:23:25 -0800

    clean data:

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
 import pandas as pd
 import requests as re
 
 full_df = pd.DataFrame()
 
-def get_recent_sales(url, product):
+def get_recent_sales(url, product, keyword):
   global full_df
   
   df_former = pd.read_csv('data/' + product + '.csv', names=['date', 'price', 'title'], header=None)
@@ -26,7 +27,10 @@ def get_recent_sales(url, product):
       date = item.find('div', {'class': 's-item__title--tagblock'}).find('span', recursive=False).contents[0].replace('Sold ', '')
     except:
       print('error')
-    if price is not None:
+    if price is not None and keyword in title.lower():
+      price = price.replace('$', '')
+      date = parse(date)
+      print(date)
       df.append({'date': date, 'price': price,  'title': title})
   print(pd.DataFrame(df))
   
@@ -37,11 +41,11 @@ def get_recent_sales(url, product):
   temp['product'] = product
   full_df = full_df.append(temp)
 
-get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=Nikkor+10-24mm+f%2F3.5-4.5G+ED&_sacat=0&LH_TitleDesc=0&_odkw=Nikon+AF-S+DX+Zoom-Nikkor+10-24mm+f%2F3.5-4.5G+ED&_osacat=0&LH_Complete=1&LH_Sold=1', 'nikkor1024')
+get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=Nikkor+10-24mm+f%2F3.5-4.5G+ED&_sacat=0&LH_TitleDesc=0&_odkw=Nikon+AF-S+DX+Zoom-Nikkor+10-24mm+f%2F3.5-4.5G+ED&_osacat=0&LH_Complete=1&LH_Sold=1', 'nikkor1024', '10-24')
 
-get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=nikon+d7200&_sacat=0&LH_TitleDesc=0&_odkw=Nikkor+10-24mm+f%2F3.5-4.5G+ED&_osacat=0&LH_Complete=1&LH_Sold=1', 'd7200')
+get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=nikon+d7200&_sacat=0&LH_TitleDesc=0&_odkw=Nikkor+10-24mm+f%2F3.5-4.5G+ED&_osacat=0&LH_Complete=1&LH_Sold=1', 'd7200', 'd7200')
 
-get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=tamron+24-70+nikon+g1&_sacat=0&LH_TitleDesc=0&_odkw=nikon+d7200&_osacat=0&LH_Complete=1&LH_Sold=1', 'tamron2470')
+get_recent_sales('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=tamron+24-70+nikon+g1&_sacat=0&LH_TitleDesc=0&_odkw=nikon+d7200&_osacat=0&LH_Complete=1&LH_Sold=1', 'tamron2470', '24-70')
 
 full_df.to_csv('data/full_data.csv')
 
